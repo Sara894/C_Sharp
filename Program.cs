6dddd5b9aed559f1cff5db98f2c9cb6fe8650aff
s.ischanova <s.ischanova@thecoders.ru>
2021-07-15 13:03:35 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Собака Серая,\n F");
+        }
+    }
+}
