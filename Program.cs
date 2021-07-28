9136b2494538a436a28b02d66bba6a11ebe6fb7d
s.ischanova <s.ischanova@thecoders.ru>
2021-07-28 22:33:57 +0500

    task1

--- Program.cs
+++ Program.cs
@@ -6,14 +6,21 @@ namespace C_Sharp
     {
        static void Main(string[] args)
        {
-           double a = 9;
-           var result = GetResult( a);
+         /*   Console.WriteLine("Введите число а: ");
+           double a = Convert.ToDouble(Console.ReadLine()); */
+           double a = 10;
+           var result = GetResult(a);
            Console.WriteLine(result);
        }
 
        static double GetResult(double a)
        {
-           double result = (5 - a);
+           double a2 = Math.Pow(a, 2);
+           double x = (1 + a + a2)/(2*a + a2);
+           double y = (1 - a + a2)/(2*a - a2);
+           double xy = x + 2 -y;
+           double fraction = Math.Pow(xy,-1);
+           double result = fraction*(5 - a2);
 
            return result;
        }
