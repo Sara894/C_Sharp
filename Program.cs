2d6ab76d5f052e8247bb7e5686d2ef3eb675121e
s.ischanova <s.ischanova@thecoders.ru>
2021-07-28 21:59:15 +0500

    начало

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ namespace C_Sharp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       static void Main(string[] args)
+       {
+           double a = 9;
+           var result = GetResult( a);
+           Console.WriteLine(result);
+       }
+
+       static double GetResult(double a)
+       {
+           double result = (5 - a);
+
+           return result;
+       }
     }
 }
