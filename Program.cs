54ce0630a41da4bc7cdda07a053290eeda9e0bfa
s.ischanova <s.ischanova@thecoders.ru>
2021-07-30 10:16:59 +0500

    куча функций

--- Program.cs
+++ Program.cs
@@ -17,29 +17,46 @@ namespace Task3
             GetSideFromA(5);
             GetLittleRadiusFromA(4);
         }
-
+        //Вычисление по одной стороне
+        //сторона 
         static void GetSideFromA(double a)
         {
             Console.WriteLine("Стороны треугольника равны: " + a);
         }
-         static void GetLittleRadiusFromA(double a)
+          //высота 
+         static void GetHeightFromA(double a)
         {
-            Console.WriteLine("Радиус вписанной окружности: " + ((a*Math.Pow(3,0.5))/4));
+            Console.WriteLine("Высота равностороннего треугольника: " + (a*Math.Pow(3,0.5))/2);
         }
-        
-        static void GetBigRadiusFromA(double a)
-        {
-            Console.WriteLine("Радиус описанной окружности: " + a);
-        }
-        
+        //площадь 
         static void GetSquareFromA(double a)
         {
             Console.WriteLine("Площадь равностороннего треугольника: " + (a*a*Math.Pow(3,0.5))/4);
         } 
-
-         static void GetHeightFromA(double a)
+        //радиус вписанной 
+         static void GetLittleRadiusFromA(double a)
         {
-            Console.WriteLine("Высота равностороннего треугольника: " + (a*Math.Pow(3,0.5))/2);
-        } 
+            Console.WriteLine("Радиус вписанной окружности: " + ((a*Math.Pow(3,0.5))/4));
+        }
+        //радиус описанной
+        static void GetBigRadiusFromA(double a)
+        {
+            Console.WriteLine("Радиус описанной окружности: " + a);
+        }
+
+        //вычисление через высоту
+        //сторона
+        static double GetSideFromH(double h)
+        {
+            double a = (2.0*h)/Math.Pow(3,0.5);
+            Console.WriteLine("Стороны треугольника равны: " + a);
+            return a;
+        }
+        //площадь
+         static void GetSquareFromH(double h)
+        {
+            double a =  GetSideFromH(h);
+            Console.WriteLine("Площадь равностороннего треугольника:: " + (h*a)/2.0 );
+        }
     }
 }
