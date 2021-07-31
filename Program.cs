b7cc5f97f2c0ebca972f0b3e06d6c63c3e2c127e
s.ischanova <s.ischanova@thecoders.ru>
2021-07-31 20:59:16 +0500

    4 задачка решена для а

--- Program.cs
+++ Program.cs
@@ -1,36 +1,50 @@
 ﻿using System;
-
+/* Доказать (путем перебора возможных значений), что для любых величин
+ А, В, С типа bool следующие пары логических выражений имеют одинаковые значения (эквивалентны): 
+ а) A OR B и B OR А; 
+б) A AND В И В AND A; 
+в) (A OR В) OR С И A OR С; 
+г) (A AND В) AND С И A AND (В AND С) ; 
+д) A AND (A OR В) И А; 
+е) A OR (A AND В) И А; 
+ж) A AND (В OR С) И (A AND В) OR (A AND С); 
+з) A OR (В AND С) И (A OR В) AND (A OR С).
+*/
 namespace Task4
 {
     class Program
     {
-        static void Main (string[] args)
-        { 
-           //double stip ;
-          // double renta = 10000;
-         //  int year = 10;
-         int month = 0;
-         double costs = 0;
-         while(month < 9)
-         {
-             costs +=  GetGrowUpPrice(10000, month);
-            Console.WriteLine( GetGrowUpPrice(10000, month));
-             month++;
-         }
-           Console.WriteLine("Это столько");
-           Console.WriteLine(costs);
-           Console.ReadKey();
+        static bool Right(bool A, bool B)
+        {
+            return A || B;
         }
 
-        static double GetGrowUpPrice(double beginPrice, int countOfMonth)
+        static bool Left(bool A, bool B)
         {
-             double newPrice = beginPrice;
-            while(countOfMonth>0)
-            {
-                 newPrice = 1.03 * newPrice;
-                countOfMonth--;
-            }
-            return newPrice;
+            return B || A;
         }
+
+        static void Main(string[] args)
+        {
+            bool A = true;
+            bool B = true;
+            bool truth = true;
+            //true;
+            truth &= Left(A,B) == Right(A,B);//логические умножения
+            A = true;
+            B = false;
+            truth &= Left(A,B) == Right(A,B);
+            A = false;
+            B = true;
+            truth &= Left(A,B) == Right(A,B);
+             A = false;
+            B = false;
+            truth &= Left(A,B) == Right(A,B);
+            Console.WriteLine(truth ? "Доказано" : "Не доказано");
+            Console.ReadKey();
+
+        }
+
+      
     }
 }
