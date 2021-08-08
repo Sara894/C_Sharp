375828ae76791a02d3f57fa5e546946f2b214d9d
s.ischanova <s.ischanova@thecoders.ru>
2021-08-08 19:55:40 +0500

    1 сделано супер

--- Program.cs
+++ Program.cs
@@ -7,12 +7,13 @@ namespace Task1
     {
       static void Main(string[] args)
       {
-          int n = int.Parse(Console.ReadLine());
+         Console.WriteLine(307*307);
+          int n =100000;
           int i = 10;
           while (i<n)
           {
             
-              bool s = IsPalindrome(i*i);
+              bool s = IsPalindrome(i*i, i);
               if (s)
               {
                   Console.WriteLine("Палиндром дает -- "+i);
@@ -20,17 +21,62 @@ namespace Task1
               i++;
           }
           Console.ReadKey();
+         
       }
 
-      static bool IsPalindrome(int square)
+      static bool IsPalindrome(int square, int i)
       {
-         string str = square.ToString();
-         if (str[0] == str[2])
-         {
-             return true;
-         }
-         return false;
+        /* square = 121;
+        i = 11;
+        121 = 100*1 + 2*10 + 1;
+        if (1 == 1)
+          return true; */
+          //Console.WriteLine("Square  " + (square ) + "  i = "+i+"\n");
+          int square_div_100 = square / 100;
+            int hundred_mod = square / 100;
+          int ten_mod = square % 10;
+        if (square_div_100< 10)
+        {
+            
+          if (hundred_mod == ten_mod)
+          {
+              return true;
+          }
+        
+        return false;
+      }
+      else if (square_div_100 > 10)
+      {
+         return IsPalindromeForBigNumber( square);
+      }
+      else
+      {
+          return false;
       }
       
+    }
+
+    static bool IsPalindromeForBigNumber(int square)
+    {
+      
+      string squareStr = square.ToString();
+      char[] straight = squareStr.ToCharArray();
+      char[] reverse = squareStr.ToCharArray();
+       Array.Reverse(reverse);
+        int s =0;
+       for (int i = 0; i < reverse.Length; ++i)
+       {
+        
+         if( straight[i] == reverse[i])
+           s++;
+        
+       }
+       if (s == reverse.Length)
+         return true;
+      else
+        return false;
+       }
+     
+     
     }
 }
