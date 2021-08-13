7c73877e8022a4100a6bc0797f06bc553c27038f
s.ischanova <s.ischanova@thecoders.ru>
2021-08-13 10:34:46 +0500

    try catch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-/* Найти целые числа, которые при возведении в квадрат дают палиндромы, например 26^2= 676.
+/* Дана целочисленная таблица А[n].
+ Найти наименьшее число K элементов, которые можно выкинуть
+  из данной последовательности,
+ так чтобы осталась  возрастающая подпоследовательность
+ (от меньшего к большему).
 */
 namespace Task2
 {
@@ -7,76 +11,41 @@ namespace Task2
     {
       static void Main(string[] args)
       {
-         Console.WriteLine(307*307);
-          int n =100000;
-          int i = 10;
-          while (i<n)
-          {
-            
-              bool s = IsPalindrome(i*i, i);
-              if (s)
+          int[] arr = {555,1,2,3,777};
+          Console.WriteLine("Исходный массив: ");
+         
+         /*  foreach (int elem in arr)
+            {
+               Console.Write("\t"+elem);
+            }
+             Console.WriteLine("_________________"); */
+      
+      
+           for (int i = 0; i < arr.Length; i++)
+         {
+             try
+           {
+             if(arr[i]>arr[i+1] )
               {
-                  Console.WriteLine("Палиндром дает -- "+i);
+                 Console.WriteLine(arr[i]);
               }
-              i++;
-          }
+              else
+              {
+                Console.WriteLine("HHHHH "+i);
+              }
+           }
+            catch(IndexOutOfRangeException)
+        {
+          Console.WriteLine("Hello, world!");
+        }
+         }
+        
+      
+        
           Console.ReadKey();
          
       }
 
-      static bool IsPalindrome(int square, int i)
-      {
-        /* square = 121;
-        i = 11;
-        121 = 100*1 + 2*10 + 1;
-        if (1 == 1)
-          return true; */
-          //Console.WriteLine("Square  " + (square ) + "  i = "+i+"\n");
-          int square_div_100 = square / 100;
-            int hundred_mod = square / 100;
-          int ten_mod = square % 10;
-        if (square_div_100< 10)
-        {
-            
-          if (hundred_mod == ten_mod)
-          {
-              return true;
-          }
-        
-        return false;
-      }
-      else if (square_div_100 > 10)
-      {
-         return IsPalindromeForBigNumber( square);
-      }
-      else
-      {
-          return false;
-      }
-      
     }
-
-    static bool IsPalindromeForBigNumber(int square)
-    {
-      
-      string squareStr = square.ToString();
-      char[] straight = squareStr.ToCharArray();
-      char[] reverse = squareStr.ToCharArray();
-       Array.Reverse(reverse);
-        int s =0;
-       for (int i = 0; i < reverse.Length; ++i)
-       {
         
-         if( straight[i] == reverse[i])
-           s++;
-        
-       }
-       if (s == reverse.Length)
-         return true;
-      else
-        return false;
-       }
-     
-     
-    }
 }
