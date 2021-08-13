3d9c35270df5637fbe2026fa54702d9d476715af
s.ischanova <s.ischanova@thecoders.ru>
2021-08-13 11:03:59 +0500

    не получается

--- Program.cs
+++ Program.cs
@@ -9,43 +9,41 @@ namespace Task2
 {
     class Program
     {
-      static void Main(string[] args)
-      {
-          int[] arr = {555,1,2,3,777};
-          Console.WriteLine("Исходный массив: ");
-         
-         /*  foreach (int elem in arr)
-            {
-               Console.Write("\t"+elem);
-            }
-             Console.WriteLine("_________________"); */
-      
-      
-           for (int i = 0; i < arr.Length; i++)
-         {
-             try
-           {
-             if(arr[i]>arr[i+1] )
-              {
-                 Console.WriteLine(arr[i]);
-              }
-              else
-              {
-                Console.WriteLine("HHHHH "+i);
-              }
-           }
-            catch(IndexOutOfRangeException)
+        static void Main(string[] args)
         {
-          Console.WriteLine("Hello, world!");
+            int[] arr = { 555, 999, 7, 3, 777 };
+           // Console.WriteLine("Исходный массив: ");
+
+            /*  foreach (int elem in arr)
+               {
+                  Console.Write("\t"+elem);
+               }
+                Console.WriteLine("_________________"); */
+
+
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                try
+                {
+                  if (arr[i] > arr[i + 1])
+                   {
+                      Console.WriteLine("Надо бы выкинуть элемент " + arr[i] + " под номером " + i);
+                   }
+            
+                }
+                catch(IndexOutOfRangeException)
+                {
+                  Console.WriteLine("IndexOutOfRangeException");
+                }
+            }
+
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
         }
-         }
-        
-      
-        
-          Console.ReadKey();
-         
-      }
 
     }
-        
+
 }
