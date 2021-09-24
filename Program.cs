d87539ab3a77969f1ba41091a43bd3ee68a8dafb
Sara894 <t48250klo@gmail.com>
2021-09-24 18:02:48 +0300

    1 задание палиндром

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-/* 8. Доказать (путем перебора возможных значений),
- что для любых величин А, В, С типа bool следующие 
- пары логических выражений имеют одинаковые значения (эквивалентны):   
- д) A AND (A OR В) И А */
+/* Найти целые числа, которые при возведении в квадрат дают палиндромы, например 
+262 
+262 
+ 
+= 676*/
 namespace Task3
 {
     class Program
@@ -10,32 +11,24 @@ namespace Task3
     
         static void Main(string[] args)
         {
-            bool truth = true;
-            bool[] arr = new bool[2] {true,false};
-            foreach(bool A in arr)
+            Console.WriteLine("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            int temp = num ;
+            int reverseNum = 0;
+            while(num > 0)
             {
-                foreach(bool B in arr)
-                {
-                    truth &= Right(A,B) == Left(A,B);
-                }
+                int digit = num % 10;
+                reverseNum = reverseNum * 10 + digit;
+                num /=10;
             }
-    
-            if(truth)
-            {
-                Console.WriteLine("Доказано");
-            }
+            Console.WriteLine("Перевернутое число: " + reverseNum);
+
+            if (temp == reverseNum)
+               Console.WriteLine("Это палиндром");
+            else
+               Console.WriteLine("Не палиндром");
             Console.ReadKey();
         }
 
-        static bool Right(bool A, bool B)
-        {
-            return A && (A || B);
-        }
-
-         static bool Left(bool A, bool B)
-        {
-            return A ;
-        }
-      
     }
     }
