0467977b8651fa352e5cf3724dda20f4d472ff32
Sara894 <t48250klo@gmail.com>
2021-10-05 22:54:24 +0300

    Ошибочка вышла

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 /* Дана целочисленная таблица А[n]. Найти наименьшее число K элементов, 
-которые можно выкинуть из данной последовательности, так чтобы осталась  возрастающая подпоследовательность.  */
+которые можно выкинуть из данной последовательности, так чтобы осталась  возрастающая подпоследовательность.  
+Это все равно что найти длину наибольшей возрастающей подпоследовательности и отнять ее из длины массива
+Нет, не все равно. См код ниже*/
 namespace Task3
 {
     class Program
@@ -9,37 +11,44 @@ namespace Task3
         static void Main(string[] args)
         {
             int[] arr = {7,2,3,4,1,99};
-            int[] sub = new int[arr.Length];
+            int a= GetLengthLongestSubsequence(arr);
 
-            for (int i = 0; i<(arr.Length); i++)
-            {
-               try
+        
+               Console.WriteLine(a);
+        
+            Console.ReadKey();
+        }
+
+
+
+        static int GetLengthLongestSubsequence(int[] nums)
+        {
+            if(nums.Length == 1)
+              return 1;
+
+            int[] lengthOfSubsequense = new int[nums.Length];
+
+           for(int j = 1; j < nums.Length; j++)
+           {
+               for (int k = 0; k < j; k++)
                {
-                     if(arr[i]>arr[i+1])
-                {
-                    if(arr[i]<arr[i+2])
-                    sub[i+1] = arr[i+1];
-                    else
-                    sub[i] = arr[i];
-                }
-               }
-               catch (IndexOutOfRangeException)
-               {
-                   if(arr[i-1]<arr[i])
+                   if (nums[j] > nums[k])
                    {
-                       sub[i] = arr[i];
+                       if(lengthOfSubsequense[j] <= lengthOfSubsequense[k])
+                       {
+                           lengthOfSubsequense[j] = lengthOfSubsequense[k]+1;
+                       }
                    }
                }
-            }
+           }
 
-             for (int i = 0; i<sub.Length; i++)
-            {
-               Console.WriteLine(sub[i]);
-            }
+           int maximum = 0;
 
-
-            
-            Console.ReadKey();
+           foreach(int length in lengthOfSubsequense )
+           {
+               maximum = Math.Max(maximum,length);
+           }
+           return maximum;
         }
     }
 }
