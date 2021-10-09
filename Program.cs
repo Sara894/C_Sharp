fb9285278fe567daea7065ea37b49c3820aa7bdd
Sara894 <t48250klo@gmail.com>
2021-10-09 09:28:28 +0300

    так тоже работает

--- Program.cs
+++ Program.cs
@@ -35,28 +35,31 @@ namespace sharp
 
          static int[] Sorting(int[] arr)
         {
-            
-            int[] fr = new int[2];
-            int[] fr1 = new int[2];
-            for(int i = 0; i< 2 ; i++)
+            for(int i = 0; i < (arr.Length/2 ); i++)
             {
-                if(arr[i]>arr[i+1])
+                int[] fr = new int[2];
+                int[] fr1 = new int[2];
+                for(int j = 0; j < 2; j++)
                 {
-                    fr[i] = arr[i+1];
-                    fr[i+1] = arr[i];
-                }
-            } 
-            int j = 0;
-            for(int i = 2; i< 3 ; i++)
-            {
-                if(arr[i]>arr[i+1])
+                     if(arr[j]>arr[j+1])
                 {
-                    fr1[j] = arr[i+1];
-                    fr1[j+1] = arr[i];
-                    j++;
+                    fr[j] = arr[j+1];
+                    fr[j+1] = arr[j];
                 }
-            } 
-            return Merge(fr, fr1);   
+                }
+                int t = 0;
+                for(int k = 2; k< 3; k++)
+                {
+                      if(arr[k]>arr[k+1])
+                {
+                    fr1[t] = arr[k+1];
+                    fr1[t+1] = arr[k];
+                    t++;
+                }
+                }
+                return Merge(fr, fr1); 
+            }
+            return arr;   
         }
 
     }
