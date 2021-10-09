b5b383235b1a647953dbf4a050b30e9f1c3279dd
Sara894 <t48250klo@gmail.com>
2021-10-09 08:59:21 +0300

    слияние

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace sharp
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] {4,3,2,1};
-            int[] newArr = Merge(arr,4);
+            int[] ar1 = new int[] {33,4,45,56};
+            int[] ar2 = new int[] {1,2,67,66};
+            int[] newArr = Merge(ar1,ar2);
             foreach(int val in newArr)
             {
                 Console.WriteLine(val+" ");
@@ -15,15 +16,32 @@ namespace sharp
             Console.ReadKey();
         }
 
-        static int[] Merge(int[] arr,int t)
+        static int[] Merge(int[] ar1, int[] ar2)
         {
-                for(int i = 0; i < arr.Length-1; i+=4)
+            int[] arr = new int[(ar1.Length*2)];
+                for(int j = 0; j < ar1.Length; j++)
+                {
+                    arr[j] = ar1[j];
+                }
+                int k = 0;
+                for(int i = ar1.Length; i < arr.Length; i++)
+                {
+                    arr[i] = ar2[k];
+                    k++;
+                }
+
+            return arr;   
+        }
+
+         static int[] Merge1(int[] arr)
+        {
+            
+            for(int i = 0; i < arr.Length-1; i+=2)
             {
-                for(int j = i; j < i+3; j++)
+                for(int j = i; j < i+1; j++)
                 {
                     if(arr[j]>arr[j+1])
                     {
-                       // Console.WriteLine("arr[i]" + arr[i])
                         int temp = arr[j];
                         arr[j] = arr[j+1];
                         arr[j+1] = temp;
