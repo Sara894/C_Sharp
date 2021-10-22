cfe2fb5a761ef80b2c68fcd79808de751a1e7833
s.ischanova <s.ischanova@thecoders.ru>
2021-10-22 18:48:24 +0300

    остановка работы 3

--- Program.cs
+++ Program.cs
@@ -1,83 +1,29 @@
 ﻿using System;
-/* Алгоритм фон Неймана. Упорядочить массив 
- a1, a2, …, an 
- a1, a2, …, an 
- 
- по неубыванию с помощью алгоритма сортировки слияниями: 
-
-1) каждая пара соседних элементов сливается в одну группу из двух элементов (последняя группа может состоять из одного элемента); 
-
-2) каждая пара соседних двухэлементных групп сливается в одну четырехэлементную группу и т.д. 
-
-При каждом слиянии новая укрупненная группа упорядочивается. */
+/* Подсчитать количество строк заданной целочисленной матрицы n×n,
+ являющихся перестановкой чисел 1, 2, ..., n
+ (т.е. содержащих каждое из чисел 1, 2, ..., n ровно один раз).  */
 namespace Task3
 {
     class Program
     {
-    
+
         static void Main(string[] args)
         {
-            int[] A = new int[16];
-            A = Enter(A);
-
-            int[] B = SortNeiman(A);
-            Show(B);
+            int[] a = new int[8]{2,1,5,4,10,6,90,5};
+            int[] b = new int[8];
+            for(int i = 0; i < A.Length;i++)
+            {
+              for(int j = 0; j < A.Length; j++)
+              {
+                int[] temp = new int[2];
+                if(a[i]>a[i+1])
+                {
+                  temp[j] = a[i+1];
+                  temp[j+1] = a[i];
+                }
+              }
+            }
             Console.ReadKey();
         }
-
-
-        static void Show(int[] nums)
-        {
-            // вывод
-            Console.WriteLine();
-            Console.WriteLine("Вывод отсортированного массива");
-            for (int i = 0; i < nums.Length; i++)
-            {
-                Console.Write(nums[i] + " ");
-            }
-        }
-
-        static int[] Enter(int[] A)
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < A.Length; i++)
-            {
-                A[i] = rnd.Next(0, 100);
-                Console.Write(A[i] + " ");
-            }
-            return A;
-        }
-
-        static int[] SortNeiman(int[] A)
-        {
-            int[] B = new int[A.Length];
-            int index = 2;
-            while (index != A.Length*2)
-            {
-                int[] temp = new int[index];
-                index--;
-                for (int i = 0; i < A.Length; i++)
-                {
-                    if (i % index == 0 && i!=0)
-                    {
-                        for (int j = 0; j < temp.Length; j++)
-                        {
-                            temp[j] = A[i - j];
-                        }
-                        Array.Sort(temp);
-                        Array.Reverse(temp);
-                        for (int j = 0; j < temp.Length; j++)
-                        {
-                            B[i - j] = temp[j];
-                        }
-                    }
-                }
-                index++;
-                index *= 2;
-            }
-            return B;
-        }
-
-    
     }
-    }
+}
