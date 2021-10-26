25d2de86edf2086e835f4f8b649346681d64ce5b
s.ischanova <s.ischanova@thecoders.ru>
2021-10-26 14:37:48 +0300

    сортировка фон неймана

--- Program.cs
+++ Program.cs
@@ -9,21 +9,55 @@ namespace Task3
 
         static void Main(string[] args)
         {
-            int[] a = new int[8]{2,1,5,4,10,6,90,5};
+            int[] a = new int[8]{100,23,3333,33,333111,4,0,1};
             int[] b = new int[8];
-            for(int i = 0; i < A.Length;i++)
+            b = sortJon(a);
+            foreach(int val in b)
             {
-              for(int j = 0; j < A.Length; j++)
-              {
-                int[] temp = new int[2];
-                if(a[i]>a[i+1])
-                {
-                  temp[j] = a[i+1];
-                  temp[j+1] = a[i];
-                }
-              }
+              Console.Write(val+" ");
             }
             Console.ReadKey();
+        }
+
+        static int[] sortJon(int[] data)
+        {
+          int m = 1;
+          while(m<data.Length)
+          {
+            int j = 0;
+            while(j+m < data.Length)
+            {
+              sortJonMerge(j,m,m, data);
+              j = j + m + m;
+            }
+            m = m+m;
+          }
+          return data;
+        }
+
+        static int[] sortJonMerge(int j, int r, int m,int[] data)
+        {
+          if(j + r < data.Length)
+          {
+            if (m == 1)
+             {
+               if(data[j]>data[j+r])
+               {
+                 int temp = data[j];
+                 data[j] = data[j + r];
+                 data[j+r] =  temp;
+               }
+             }
+            else
+            {
+              m = m / 2;
+              sortJonMerge (j,r,m,data);
+              if(j+r+m < data.Length)
+                sortJonMerge(j + m, r, m,data);
+              sortJonMerge(j + m, r - m, m,data);
+            }
+          }
+          return data;
         }
     }
 }
