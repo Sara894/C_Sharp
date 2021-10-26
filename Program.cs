e6f56b7db70a449d2a07442a1f60e4ab29a87bec
s.ischanova <s.ischanova@thecoders.ru>
2021-10-26 18:46:59 +0300

    1 задачка для одного массива

--- Program.cs
+++ Program.cs
@@ -9,55 +9,33 @@ namespace Task3
 
         static void Main(string[] args)
         {
-            int[] a = new int[8]{100,23,3333,33,333111,4,0,1};
-            int[] b = new int[8];
-            b = sortJon(a);
-            foreach(int val in b)
+            int[,] m = { { 1, 2, 3 }, { 1, 3, 2 }, { 3, 2, 1 } };
+            int[] arr = new int[]{1,2,3};
+            int sum = 0;
+            int num = 0;
+            for (int i = 0; i < 3; i++)
             {
-              Console.Write(val+" ");
+                for (int j = 0; j < 3; j++)
+                {
+                    if(Array.Exists(arr,element => element == m[i,j]))
+                    {
+                      sum +=m[i,j];
+                      if(j ==2 && sum ==6)
+                      {
+                        Console.WriteLine("num++ " +i +" sum "+ sum);
+                        num++;
+                      }
+                    }
+                    else{
+                      j = 5;
+                    }
+                }
+                sum = 0;
             }
+            Console.WriteLine(" num: "+num);
             Console.ReadKey();
         }
 
-        static int[] sortJon(int[] data)
-        {
-          int m = 1;
-          while(m<data.Length)
-          {
-            int j = 0;
-            while(j+m < data.Length)
-            {
-              sortJonMerge(j,m,m, data);
-              j = j + m + m;
-            }
-            m = m+m;
-          }
-          return data;
-        }
 
-        static int[] sortJonMerge(int j, int r, int m,int[] data)
-        {
-          if(j + r < data.Length)
-          {
-            if (m == 1)
-             {
-               if(data[j]>data[j+r])
-               {
-                 int temp = data[j];
-                 data[j] = data[j + r];
-                 data[j+r] =  temp;
-               }
-             }
-            else
-            {
-              m = m / 2;
-              sortJonMerge (j,r,m,data);
-              if(j+r+m < data.Length)
-                sortJonMerge(j + m, r, m,data);
-              sortJonMerge(j + m, r - m, m,data);
-            }
-          }
-          return data;
-        }
     }
 }
