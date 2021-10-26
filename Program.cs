3f6444a9759b9eacd5755e74683f388b93d3f236
s.ischanova <s.ischanova@thecoders.ru>
2021-10-26 21:24:26 +0300

    1 задание

--- Program.cs
+++ Program.cs
@@ -9,33 +9,92 @@ namespace Task3
 
         static void Main(string[] args)
         {
-            int[,] m = { { 1, 2, 3 }, { 1, 3, 2 }, { 3, 2, 1 } };
-            int[] arr = new int[]{1,2,3};
-            int sum = 0;
-            int num = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if(Array.Exists(arr,element => element == m[i,j]))
-                    {
-                      sum +=m[i,j];
-                      if(j ==2 && sum ==6)
-                      {
-                        Console.WriteLine("num++ " +i +" sum "+ sum);
-                        num++;
-                      }
-                    }
-                    else{
-                      j = 5;
-                    }
-                }
-                sum = 0;
-            }
-            Console.WriteLine(" num: "+num);
+            Console.WriteLine("Введите число n: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+             int[] arr = new int[n];
+             arr = getDefaultArray(n);
+             int SUM = getSUM(arr);
+            int[,] m = getMatrix(n);
+            showMatrix(m,n);
+           
+            int num = getNum(arr,m,n,SUM);
+            Console.WriteLine("Количество строк: "+num);
             Console.ReadKey();
         }
 
 
+        static int getNum(int[] arr, int[,] m, int n,int SUM)
+        {
+          int num = 0;
+          int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(Array.Exists(arr,element => element == m[i,j]))
+                    {
+                      sum +=m[i,j];
+                      if(j ==(n-1) && sum ==SUM)
+                      {
+                        num++;
+                      }
+                    }
+                    else{
+                      j = n+1 ;
+                    }
+                }
+                sum = 0;
+            }
+            return num;
+        }
+        static int[] getDefaultArray(int n)
+        {
+           int[] arr = new int[n];
+            for(int i = 0; i<n; i++)
+            {
+              arr[i] = i+1;
+            }
+            return arr;
+        }
+
+        static int getSUM(int[] arr)
+        {
+          int SUM = 0;
+            foreach(int val in arr)
+            {
+              SUM+=val;
+              Console.Write(val + " ");
+            }
+            Console.WriteLine(" ");
+            return SUM;
+        }
+
+        static int[,] getMatrix(int n)
+        {
+          int[,] m = new int[n,n];
+            Random rand = new Random();
+            for(int i  = 0; i < n ; i ++)
+            {
+              for(int j  = 0; j < n ; j ++)
+              {
+               // m[i,j] = rand.Next(1,n);
+               Console.WriteLine("Заполните матрицу: ");
+               m[i,j] = Convert.ToInt16(Console.ReadLine());
+              }
+            }
+            return m;
+        }
+
+        static void showMatrix(int[,] m, int n)
+        {
+           for(int i  = 0; i < n ; i ++)
+            {
+              for(int j  = 0; j < n ; j ++)
+              {
+                Console.Write(m[i,j] + " ");
+              }
+              Console.WriteLine(" ");
+            }
+        }
     }
 }
