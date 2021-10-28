c914b8dee79f4e3265bab6ed925209b39ce3028b
s.ischanova <s.ischanova@thecoders.ru>
2021-10-28 20:06:33 +0300

    task 3

--- Program.cs
+++ Program.cs
@@ -12,32 +12,34 @@ namespace Task3
         static void Main(string[] args)
         {
             Queue<int> nums1 = new Queue<int>();
-            nums1.Enqueue(3); // очередь 3
-            nums1.Enqueue(5); // очередь 3, 5
-            nums1.Enqueue(8); // очередь 3, 5, 8
+            Console.WriteLine("Введите числа в порядке возрастания для первой очереди: ");
+            nums1.Enqueue(1);
+            nums1.Enqueue(3);
+            nums1.Enqueue(5);
             Queue<int> nums2 = new Queue<int>();
-            nums2.Enqueue(1); // очередь 1
-            nums2.Enqueue(2); // очередь 1, 2
-            nums2.Enqueue(4); // очередь 1,2,4
+            Console.WriteLine("Введите числа в порядке возрастания для первой очереди: ");
+            nums1.Enqueue(2);
+            nums1.Enqueue(4);
+            nums1.Enqueue(6);
             Queue<int> result = new Queue<int>();
             int c = nums1.Count + nums2.Count;
             int i = 0;
 
-            while(i<c){
-                
-                  try{
-                        if (nums1.Peek() > nums2.Peek())
-                    result.Enqueue(nums2.Dequeue());
+            while (i < c)
+            {
+                if (nums1.Count > 0 && nums2.Count > 0)
+                {
+                    if (nums1.Peek() > nums2.Peek())
+                        result.Enqueue(nums2.Dequeue());
+                    else
+                        result.Enqueue(nums1.Dequeue());
+                }
                 else
-                    result.Enqueue(nums1.Dequeue());
-                  }
-
-                catch (InvalidOperationException)
                 {
                     if (nums1.Count == 0)
                         result.Enqueue(nums2.Dequeue());
                     else
-                       result.Enqueue(nums1.Dequeue());
+                        result.Enqueue(nums1.Dequeue());
                 }
                 i++;
             }
